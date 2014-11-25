6e755e76079907fa236aaa93df80193dcd6ab32e
TwistedUmbrella <twistedumbrella@gmail.com>
2014-11-24 21:21:16 -0500

    Update standard open-source disclaimer licensing

--- Assets/Dive/Scripts/DiveVR.cs
+++ Assets/Dive/Scripts/DiveVR.cs
@@ -1,4 +1,56 @@
-﻿using UnityEngine;
+﻿/* ====================================================================
+ * Copyright (c) 2012-2014 LoungeKatt Entertainment.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution or a referenced public source respository.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by Lounge Katt" unless
+ *    otherwise displayed by public repository entries.
+ *
+ * 4. The names "LoungeKatt", "TwistedUmbrella", and "Abandoned Cart"  
+ *    must not be used to endorse or promote products derived from this 
+ *    software without prior written permission. For written permission,
+ *    please contact admin@loungekatt.com.
+ *
+ * 5. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment or provide public:
+ *    "This product includes software developed by LoungeKatt" unless
+ *    otherwise displayed by tagged repository entries.
+ *
+ * THIS SOFTWARE IS PROVIDED BY LoungeKatt ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * The license and distribution terms for any publicly available version or
+ * derivative of this code cannot be changed.  i.e. this code cannot simply be
+ * copied and put under another distribution license
+ * [including the GNU Public License.] Content not subject to these terms is
+ * subject to to the terms and conditions of the Apache License, Version 2.0.
+ */
+
+
+using UnityEngine;
 using System.Collections;
 
 public class DiveVR : MonoBehaviour {
